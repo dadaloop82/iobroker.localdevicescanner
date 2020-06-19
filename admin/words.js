b98e095c1301d3bc03b175601fbbd764a599ce51
dadaloop82 <dadaloop82@gmail.com>
2020-06-19 22:55:38 +0200

    builder template

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"localdevicescanner adapter settings": {
+		"en": "Adapter settings for localdevicescanner",
+		"de": "Adaptereinstellungen für localdevicescanner",
+		"ru": "Настройки адаптера для localdevicescanner",
+		"pt": "Configurações do adaptador para localdevicescanner",
+		"nl": "Adapterinstellingen voor localdevicescanner",
+		"fr": "Paramètres d'adaptateur pour localdevicescanner",
+		"it": "Impostazioni dell'adattatore per localdevicescanner",
+		"es": "Ajustes del adaptador para localdevicescanner",
+		"pl": "Ustawienia adaptera dla localdevicescanner",
+		"zh-cn": "localdevicescanner的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
